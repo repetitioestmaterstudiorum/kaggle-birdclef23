98f6d096a2b2b005ba6b1d150be3bc140688e6d6
repetitioestmaterstudiorum <44611591+repetitioestmaterstudiorum@users.noreply.github.com>
2023-04-30 16:48:16 +0200

    add rnn raw audio attempt and cnn spectrogram attempt, ignore joblib files

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import os
 import torch
+import matplotlib.pyplot as plt
+import numpy as np
 
 # ---
 
@@ -30,3 +32,49 @@ def determine_device():
 
     print(f"We are using device: {device}")
     return device
+
+
+def normalize_spectrogram(spectrogram):
+    min_val = np.min(spectrogram)
+    max_val = np.max(spectrogram)
+    if max_val - min_val == 0:
+        return spectrogram
+    else:
+        return (spectrogram - min_val) / (max_val - min_val)
+    
+
+def plot_train_and_valid_loss_and_accuracy(train_losses, valid_losses, train_accuracies, valid_accuracies):
+    train_accuracies = [x.cpu() for x in train_accuracies]
+    valid_accuracies = [x.cpu() for x in valid_accuracies]
+    train_losses = [x.cpu() for x in train_losses]
+    valid_losses = [x.cpu() for x in valid_losses]
+
+    _, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 5))
+    if len(train_losses) == 1 and len(valid_losses) == 1:
+        ax1.scatter(0, train_losses[0], label='Training Loss')
+        ax1.scatter(0, valid_losses[0], label='Validation Loss')
+    else:
+        ax1.plot(train_losses, label='Training Loss')
+        ax1.plot(valid_losses, label='Validation Loss')
+    ax1.set_xlabel('Epoch')
+    ax1.set_ylabel('Loss')
+    ax1.legend()
+    if len(train_accuracies) == 1 and len(valid_accuracies) == 1:
+        ax2.scatter(0, train_accuracies[0], label='Training Accuracy')
+        ax2.scatter(0, valid_accuracies[0], label='Validation Accuracy')
+    else:
+        ax2.plot(train_accuracies, label='Training Accuracy')
+        ax2.plot(valid_accuracies, label='Validation Accuracy')
+    ax2.set_xlabel('Epoch')
+    ax2.set_ylabel('Accuracy')
+    ax2.legend()
+    plt.show()
+
+
+def plot_minibatch_loss(minibatch_loss):
+    plt.plot(range(len(minibatch_loss)), minibatch_loss)
+    plt.title('Minibatch Loss')
+    plt.ylabel('Cross Entropy')
+    plt.xlabel('Minibatch')
+    plt.grid()
+    plt.show()
